b65f20d6426d81847f6ab1948baacb7b607f2934
Gláucio R. Vivian <grvivian@gmail.com>
2017-05-07 13:29:56 -0300

    Add files via upload

--- src/main/java/br/com/grvivian/hibernate/TpDB.java
+++ src/main/java/br/com/grvivian/hibernate/TpDB.java
@@ -5,7 +5,18 @@ package br.com.grvivian.hibernate;
  * @author glaucio
  */
 public enum TpDB {
-  MYSQL,
-  POSTGRES,
-  FIREBIRD
+  MYSQL("MySQL"),
+  POSTGRES("PostgreSQL"),
+  FIREBIRD("Firebir");
+
+  private final String label;
+
+  private TpDB(String label) {
+    this.label = label;
+  }
+
+  @Override
+  public String toString() {
+    return this.label;
+  }
 }
